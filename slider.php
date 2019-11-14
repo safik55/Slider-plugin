19980dcdc6a25f8af61be88f5aaa0979d763b53e
viru <safik.dds@gmail.com>
2019-11-14 17:24:40 +0530

    update

--- slider.php
+++ slider.php
@@ -3,5 +3,26 @@
   Plugin Name: Custom slider 
  */
  
- echo "HIi";
+ 
+ /**
+ * Register a custom menu page.
+ */
+function wpdocs_register_my_custom_menu_page() {
+    add_menu_page(
+        __( 'Custom Slider', 'textdomain' ),
+        'Custom Slider',
+        'manage_options',
+        'custom_slider',
+        'cutom_theme_option',
+        '',
+        60
+    );
+}
+add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
+
+
+function cutom_theme_option()
+{
+	include_once(plugin_dir_url( __FILE__ ).'Slider-plugin/slider_content.php');
+}
 ?>
